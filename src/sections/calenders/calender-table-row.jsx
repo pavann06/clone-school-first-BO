b6197178efecbba747778c2734d50f9a06d4bbee
pavann06 <pavan.06.p@gmail.com>
2024-12-31 22:32:14 +0530

    calender date

--- src/sections/calenders/calender-table-row.jsx
+++ src/sections/calenders/calender-table-row.jsx
@@ -1,121 +1,4 @@
-// import PropTypes from 'prop-types';
-// import React, { useCallback } from 'react';
 
-// import Link from '@mui/material/Link';
-// import { TableRow, MenuItem, TableCell, IconButton } from '@mui/material';
-
-// import { useRouter } from 'src/routes/hooks';
-
-// import Iconify from 'src/components/iconify';
-// import CustomPopover, { usePopover } from 'src/components/custom-popover';
-
-// function CalenderTableRow({ row, onEditRow, onDeleteRow, serialNumber }) {
-//   const popover = usePopover();
-//   const router = useRouter();
-
-//   const handleViewRow = useCallback(
-//     (id) => {
-//       router.push(`/dashboard/calender/view/${id}`);
-//     },
-//     [router]
-//   );
-
-//   return (
-//     <>
-//       <TableRow hover>
-//         <TableCell>{serialNumber}</TableCell>
-
-//         <TableCell
-//           align="center"
-//           onClick={() => handleViewRow(row.id)}
-//           style={{ cursor: 'pointer' }}
-//         >
-//           {row.prompt || 'No Prompt'}
-//         </TableCell>
-//         <TableCell align="center">{row.benefit || 'No Benefit'}</TableCell>
-//         <TableCell align="center">{row.description || 'No Description'}</TableCell>
-//         <TableCell align="center">
-//           {row.youtube_video_url ? (
-//             <Link href={row.youtube_video_url} target="_blank" rel="noopener noreferrer">
-//               {row.youtube_video_url}
-//             </Link>
-//           ) : (
-//             'No URL'
-//           )}
-//         </TableCell>
-//         <TableCell align="center">
-//           {row.date ? new Date(row.date).toLocaleDateString('en-GB') : 'N/A'}
-//         </TableCell>
-//         <TableCell align="center">
-//           {row.image ? (
-//             <img
-//               src={row.image}
-//               alt={`Thumbnail for ${row.prompt}`}
-//               style={{ maxWidth: 100, maxHeight: 50 }}
-//             />
-//           ) : (
-//             'No Image'
-//           )}
-//         </TableCell>
-//         <TableCell>
-//           <IconButton color={popover.open ? 'primary' : 'default'} onClick={popover.onOpen}>
-//             <Iconify icon="eva:more-vertical-fill" />
-//           </IconButton>
-//         </TableCell>
-//       </TableRow>
-
-//       <CustomPopover
-//         open={popover.open}
-//         onClose={popover.onClose}
-//         arrow="right-top"
-//         sx={{ width: 140 }}
-//       >
-//         <MenuItem
-//           onClick={() => {
-//             onEditRow();
-//             popover.onClose();
-//           }}
-//         >
-//           <Iconify icon="solar:pen-bold" />
-//           Edit
-//         </MenuItem>
-//         <MenuItem
-//           onClick={() => {
-//             onDeleteRow();
-//             popover.onClose();
-//           }}
-//         >
-//           <Iconify icon="material-symbols:delete" />
-//           Delete
-//         </MenuItem>
-//       </CustomPopover>
-//     </>
-//   );
-// }
-
-// CalenderTableRow.propTypes = {
-//   row: PropTypes.shape({
-//     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-//     prompt: PropTypes.string,
-//     benefit: PropTypes.string,
-//     description: PropTypes.string,
-//     youtube_video_url: PropTypes.string,
-//     date: PropTypes.string,
-//     image: PropTypes.string,
-//   }),
-//   serialNumber: PropTypes.number, // Add this line
-//   onEditRow: PropTypes.func,
-//   onDeleteRow: PropTypes.func,
-// };
-
-// CalenderTableRow.defaultProps = {
-//   row: { id: null },
-//   serialNumber: null, // Add a default value if necessary
-//   onEditRow: () => {},
-//   onDeleteRow: () => {},
-// };
-
-// export default CalenderTableRow;
 
 
 import PropTypes from 'prop-types';
@@ -199,9 +82,10 @@ function CalenderTableRow({ row, onEditRow, onDeleteRow, serialNumber }) {
         </TableCell>
 
         {/* Date */}
-        <TableCell align="center">
-          {row.date ? new Date(row.date).toLocaleDateString('en-GB') : 'N/A'}
+        <TableCell>
+          {row.date}
         </TableCell>
+      
 
         {/* Image */}
         <TableCell align="center">
