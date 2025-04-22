1ae75d1842e3456d61aaa8498b192c95efe72e1b
pavann06 <pavan.06.p@gmail.com>
2025-04-22 15:17:40 +0530

    news

--- src/sections/news/view/news-list-view.jsx
+++ src/sections/news/view/news-list-view.jsx
@@ -1,209 +1,32 @@
-// // import React, { useState, useEffect } from 'react';
-// import { useQuery } from '@tanstack/react-query';
-// import React, { useState, useEffect, useCallback } from 'react';
-// import { useNavigate } from 'react-router-dom';
-
-// import {
-//   Box,
-//   Card,
-//   Table,
-//   Skeleton,
-//   Button,
-//   Container,
-//   TableBody,
-//   TableContainer,
-//   TablePagination,
-// } from '@mui/material';
-
-// import { paths } from 'src/routes/paths';
-// import { useRouter } from 'src/routes/hooks';
-// import { RouterLink } from 'src/routes/components';
-// import Iconify from 'src/components/iconify';
-
-// import request from 'src/api/request';
-
-// import Scrollbar from 'src/components/scrollbar';
-// import { useSnackbar } from 'src/components/snackbar';
-// import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
-// import { TableNoData, TableHeadCustom } from 'src/components/table';
-
-// import NewsTableRow from '../news-table-row';
-// import NewsCategoriesDropdown from '../news-categories-dropdown';
-
-// const TABLE_HEAD = [
-//   { id: 'index', label: 'Serial No' },
-//   { id: 'title', label: 'Title' },
-//   { id: 'description', label: 'Description' },
-  
-
-//   { id: 'categories', label: 'Categories ' },
-
-//   { id: 'images', label: 'Images' },
-
-//   { id: 'status', label: 'Status' },
-//   { id: 'actions ', label: 'Actions' },
-// ];
-
-// export default function NewsListView() {
-//   const router = useRouter();
-//   const { enqueueSnackbar } = useSnackbar();
-
-//   const [tableData, setTableData] = useState([]);
-//   const [totalCount, setTotalCount] = useState(0);
-//   const [pagination, setPagination] = useState({ page: 1, page_size: 10 });
-
-
-
-//   const navigate = useNavigate();
-
-//   const { data, isLoading, error } = useQuery({
-//     queryKey: ['edutainment', pagination.page, pagination.page_size],
-//     queryFn: async () => {
-//       try {
-//         const response = await request.get(
-//           `backoffice/news?page=${pagination.page}&page_size=${pagination.page_size}`
-//         );
-//         return response;
-//       } catch (err) {
-//         if (err.response && err.response.status === 401) {
-//           console.error('Unauthorized! Redirecting to login...');
-//           navigate('/auth/login?returnTo=%2Fdashboard'); // Redirect user to login page
-//         }
-//         throw err;
-//       }
-//     },
-//     keepPreviousData: true,
-//     onError: (err) => {
-//       console.error('Query error:', err);
-//     },
-//   });
-
-//   // Set data when fetched successfully
-//   useEffect(() => {
-//     if (data) {
-//       if (data?.data?.length > 0) {
-//         setTableData(data.data);
-//         setTotalCount(data.total);
-//       } else {
-//         setTableData([]);
-//         setTotalCount(0);
-//       }
-//     }
-//   }, [data]);
-
-//   const handlePageChange = (event, newPage) => {
-//     setPagination((prev) => ({ ...prev, page: newPage + 1 }));
-//   };
-
-//   // Handle change in number of rows per page
-//   const handleRowsPerPageChange = (event) => {
-//     const newPageSize = parseInt(event.target.value, 10);
-//     setPagination({ page: 1, page_size: newPageSize });
-//   };
-
-//   const handleEditRow = useCallback(
-//     (id) => {
-//       router.push(paths.dashboard.news.edit(id));
-//     },
-//     [router]
-//   );
-
-//   const handleDeleteRow = async (id) => {
-//     const response = await request.delete(`backoffice/news/${id}`);
-
-//     const { success } = response;
-
-    
-//     if (success) {
-//       enqueueSnackbar('Deleted successfully');
-
-//       // refetch the data
-//       setPagination((prev) => ({ ...prev, page: 1 }));
-//     }
-//   };
-
-//   return (
-//     <Container maxWidth="lg">
-//       <Box sx={{ position: 'relative', mb: { xs: 3, md: 5 } }}>
-//         <CustomBreadcrumbs
-//           heading="List"
-//           links={[
-//             { name: 'Dashboard', href: paths.dashboard.root },
-//             {
-//               name: 'News',
-//               href: paths.dashboard.news.root,
-//             },
-//             { name: 'List' },
-//           ]}
-//         />
-//         <Button
-//           component={RouterLink}
-//           href={paths.dashboard.news.new}
-//           variant="contained"
-//           startIcon={<Iconify icon="mingcute:add-line" />}
-//           sx={{
-//             position: 'absolute',
-//             bottom: '5px',
-//             right: '5px',
-//           }}
-//         >
-//           New News
-//         </Button>
-//       </Box>
-//       <Card>
-//         <TableContainer>
-//           <Scrollbar>
-//             <Table>
-//               <TableHeadCustom headLabel={TABLE_HEAD} />
-//               <TableBody>
-//                 {isLoading
-//                   ? [...Array(pagination.page_size)].map((_, index) => (
-//                       <Skeleton key={index} variant="rectangular" height={40} />
-//                     ))
-//                   : tableData.map((row, index) => (
-//                       <NewsTableRow
-//                         key={row.id}
-//                         row={{
-//                           ...row,
-//                           serial_no: (pagination.page - 1) * pagination.page_size + index + 1, // Updated serial number calculation
-//                         }}
-//                         onEditRow={() => handleEditRow(row.id)}
-//                         onDeleteRow={() => handleDeleteRow(row.id)}
-//                       />
-//                     ))}
-//                 {!isLoading && tableData.length === 0 && <TableNoData />}
-//               </TableBody>
-//             </Table>
-//           </Scrollbar>
-//         </TableContainer>
-
-//         <TablePagination
-//           component="div"
-//           count={totalCount}
-//           page={pagination.page - 1}
-//           rowsPerPage={pagination.page_size}
-//           onPageChange={handlePageChange}
-//           onRowsPerPageChange={handleRowsPerPageChange}
-//         />
-//       </Card>
-//     </Container>
-//   );
-// }
-
-
-import React, { useState, useEffect, useCallback } from 'react';
+// import React, { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Card, Table, Skeleton, Button, Container, TableBody, TableContainer, TablePagination } from '@mui/material';
+
+import {
+  Box,
+  Card,
+  Table,
+  Skeleton,
+  Button,
+  Container,
+  TableBody,
+  TableContainer,
+  TablePagination,
+} from '@mui/material';
+
 import { paths } from 'src/routes/paths';
 import { useRouter } from 'src/routes/hooks';
 import { RouterLink } from 'src/routes/components';
 import Iconify from 'src/components/iconify';
+
 import request from 'src/api/request';
+
 import Scrollbar from 'src/components/scrollbar';
 import { useSnackbar } from 'src/components/snackbar';
 import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
 import { TableNoData, TableHeadCustom } from 'src/components/table';
+
 import NewsTableRow from '../news-table-row';
 import NewsCategoriesDropdown from '../news-categories-dropdown';
 
@@ -211,10 +34,14 @@ const TABLE_HEAD = [
   { id: 'index', label: 'Serial No' },
   { id: 'title', label: 'Title' },
   { id: 'description', label: 'Description' },
-  { id: 'categories', label: 'Categories' },
+  
+
+  { id: 'categories', label: 'Categories ' },
+
   { id: 'images', label: 'Images' },
+
   { id: 'status', label: 'Status' },
-  { id: 'actions', label: 'Actions' },
+  { id: 'actions ', label: 'Actions' },
 ];
 
 export default function NewsListView() {
@@ -224,20 +51,23 @@ export default function NewsListView() {
   const [tableData, setTableData] = useState([]);
   const [totalCount, setTotalCount] = useState(0);
   const [pagination, setPagination] = useState({ page: 1, page_size: 10 });
-  const [selectedCategory, setSelectedCategory] = useState(null);  // For storing selected category
+
+
 
   const navigate = useNavigate();
 
   const { data, isLoading, error } = useQuery({
-    queryKey: ['newsList', pagination.page, pagination.page_size, selectedCategory],
+    queryKey: ['edutainment', pagination.page, pagination.page_size],
     queryFn: async () => {
       try {
-        const response = await request.get(`backoffice/news?page=${pagination.page}&page_size=${pagination.page_size}`);
+        const response = await request.get(
+          `backoffice/news?page=${pagination.page}&page_size=${pagination.page_size}`
+        );
         return response;
       } catch (err) {
         if (err.response && err.response.status === 401) {
           console.error('Unauthorized! Redirecting to login...');
-          navigate('/auth/login?returnTo=%2Fdashboard');
+          navigate('/auth/login?returnTo=%2Fdashboard'); // Redirect user to login page
         }
         throw err;
       }
@@ -248,26 +78,18 @@ export default function NewsListView() {
     },
   });
 
+  // Set data when fetched successfully
   useEffect(() => {
     if (data) {
       if (data?.data?.length > 0) {
-        // Filter data based on the selected category
-        const filteredData = data.data.filter((newsItem) =>
-          selectedCategory ? newsItem.categories.includes(selectedCategory) : true
-        );
-        setTableData(filteredData);
-        setTotalCount(filteredData.length); // Update total count based on filtered data
+        setTableData(data.data);
+        setTotalCount(data.total);
       } else {
         setTableData([]);
         setTotalCount(0);
       }
     }
-  }, [data, selectedCategory]);
-
-  const handleCategoryChange = (categoryId) => {
-    setSelectedCategory(categoryId);
-    setPagination((prev) => ({ ...prev, page: 1 })); // Reset to the first page when category changes
-  };
+  }, [data]);
 
   const handlePageChange = (event, newPage) => {
     setPagination((prev) => ({ ...prev, page: newPage + 1 }));
@@ -291,6 +113,7 @@ export default function NewsListView() {
 
     const { success } = response;
 
+    
     if (success) {
       enqueueSnackbar('Deleted successfully');
 
@@ -306,7 +129,10 @@ export default function NewsListView() {
           heading="List"
           links={[
             { name: 'Dashboard', href: paths.dashboard.root },
-            { name: 'News', href: paths.dashboard.news.root },
+            {
+              name: 'News',
+              href: paths.dashboard.news.root,
+            },
             { name: 'List' },
           ]}
         />
@@ -324,11 +150,6 @@ export default function NewsListView() {
           New News
         </Button>
       </Box>
-
-      <Box sx={{ mb: 3, width: '25%'   }}>
-        <NewsCategoriesDropdown onCategoryChange={handleCategoryChange} />
-      </Box>
-
       <Card>
         <TableContainer>
           <Scrollbar>
@@ -344,7 +165,7 @@ export default function NewsListView() {
                         key={row.id}
                         row={{
                           ...row,
-                          serial_no: (pagination.page - 1) * pagination.page_size + index + 1,
+                          serial_no: (pagination.page - 1) * pagination.page_size + index + 1, // Updated serial number calculation
                         }}
                         onEditRow={() => handleEditRow(row.id)}
                         onDeleteRow={() => handleDeleteRow(row.id)}
@@ -368,3 +189,203 @@ export default function NewsListView() {
     </Container>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect, useCallback } from 'react';
+// import { useQuery } from '@tanstack/react-query';
+// import { useNavigate } from 'react-router-dom';
+// import { Box, Card, Table, Skeleton, Button, Container, TableBody, TableContainer, TablePagination } from '@mui/material';
+// import { paths } from 'src/routes/paths';
+// import { useRouter } from 'src/routes/hooks';
+// import { RouterLink } from 'src/routes/components';
+// import Iconify from 'src/components/iconify';
+// import request from 'src/api/request';
+// import Scrollbar from 'src/components/scrollbar';
+// import { useSnackbar } from 'src/components/snackbar';
+// import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
+// import { TableNoData, TableHeadCustom } from 'src/components/table';
+// import NewsTableRow from '../news-table-row';
+// import NewsCategoriesDropdown from '../news-categories-dropdown';
+
+// const TABLE_HEAD = [
+//   { id: 'index', label: 'Serial No' },
+//   { id: 'title', label: 'Title' },
+//   { id: 'description', label: 'Description' },
+//   { id: 'categories', label: 'Categories' },
+//   { id: 'images', label: 'Images' },
+//   { id: 'status', label: 'Status' },
+//   { id: 'actions', label: 'Actions' },
+// ];
+
+// export default function NewsListView() {
+//   const router = useRouter();
+//   const { enqueueSnackbar } = useSnackbar();
+
+//   const [tableData, setTableData] = useState([]);
+//   const [totalCount, setTotalCount] = useState(0);
+//   const [pagination, setPagination] = useState({ page: 1, page_size: 10 });
+//   const [selectedCategory, setSelectedCategory] = useState(null);  // For storing selected category
+
+//   const navigate = useNavigate();
+
+//   const { data, isLoading, error } = useQuery({
+//     queryKey: ['newsList', pagination.page, pagination.page_size, selectedCategory],
+//     queryFn: async () => {
+//       try {
+//         const response = await request.get(`backoffice/news?page=${pagination.page}&page_size=${pagination.page_size}`);
+//         return response;
+//       } catch (err) {
+//         if (err.response && err.response.status === 401) {
+//           console.error('Unauthorized! Redirecting to login...');
+//           navigate('/auth/login?returnTo=%2Fdashboard');
+//         }
+//         throw err;
+//       }
+//     },
+//     keepPreviousData: true,
+//     onError: (err) => {
+//       console.error('Query error:', err);
+//     },
+//   });
+
+//   useEffect(() => {
+//     if (data) {
+//       if (data?.data?.length > 0) {
+//         // Filter data based on the selected category
+//         const filteredData = data.data.filter((newsItem) =>
+//           selectedCategory ? newsItem.categories.includes(selectedCategory) : true
+//         );
+//         setTableData(filteredData);
+//         setTotalCount(filteredData.length); // Update total count based on filtered data
+//       } else {
+//         setTableData([]);
+//         setTotalCount(0);
+//       }
+//     }
+//   }, [data, selectedCategory]);
+
+//   const handleCategoryChange = (categoryId) => {
+//     setSelectedCategory(categoryId);
+//     setPagination((prev) => ({ ...prev, page: 1 })); // Reset to the first page when category changes
+//   };
+
+//   const handlePageChange = (event, newPage) => {
+//     setPagination((prev) => ({ ...prev, page: newPage + 1 }));
+//   };
+
+//   // Handle change in number of rows per page
+//   const handleRowsPerPageChange = (event) => {
+//     const newPageSize = parseInt(event.target.value, 10);
+//     setPagination({ page: 1, page_size: newPageSize });
+//   };
+
+//   const handleEditRow = useCallback(
+//     (id) => {
+//       router.push(paths.dashboard.news.edit(id));
+//     },
+//     [router]
+//   );
+
+//   const handleDeleteRow = async (id) => {
+//     const response = await request.delete(`backoffice/news/${id}`);
+
+//     const { success } = response;
+
+//     if (success) {
+//       enqueueSnackbar('Deleted successfully');
+
+//       // refetch the data
+//       setPagination((prev) => ({ ...prev, page: 1 }));
+//     }
+//   };
+
+//   return (
+//     <Container maxWidth="lg">
+//       <Box sx={{ position: 'relative', mb: { xs: 3, md: 5 } }}>
+//         <CustomBreadcrumbs
+//           heading="List"
+//           links={[
+//             { name: 'Dashboard', href: paths.dashboard.root },
+//             { name: 'News', href: paths.dashboard.news.root },
+//             { name: 'List' },
+//           ]}
+//         />
+//         <Button
+//           component={RouterLink}
+//           href={paths.dashboard.news.new}
+//           variant="contained"
+//           startIcon={<Iconify icon="mingcute:add-line" />}
+//           sx={{
+//             position: 'absolute',
+//             bottom: '5px',
+//             right: '5px',
+//           }}
+//         >
+//           New News
+//         </Button>
+//       </Box>
+
+//       <Box sx={{ mb: 3, width: '25%'   }}>
+//         <NewsCategoriesDropdown onCategoryChange={handleCategoryChange} />
+//       </Box>
+
+//       <Card>
+//         <TableContainer>
+//           <Scrollbar>
+//             <Table>
+//               <TableHeadCustom headLabel={TABLE_HEAD} />
+//               <TableBody>
+//                 {isLoading
+//                   ? [...Array(pagination.page_size)].map((_, index) => (
+//                       <Skeleton key={index} variant="rectangular" height={40} />
+//                     ))
+//                   : tableData.map((row, index) => (
+//                       <NewsTableRow
+//                         key={row.id}
+//                         row={{
+//                           ...row,
+//                           serial_no: (pagination.page - 1) * pagination.page_size + index + 1,
+//                         }}
+//                         onEditRow={() => handleEditRow(row.id)}
+//                         onDeleteRow={() => handleDeleteRow(row.id)}
+//                       />
+//                     ))}
+//                 {!isLoading && tableData.length === 0 && <TableNoData />}
+//               </TableBody>
+//             </Table>
+//           </Scrollbar>
+//         </TableContainer>
+
+//         <TablePagination
+//           component="div"
+//           count={totalCount}
+//           page={pagination.page - 1}
+//           rowsPerPage={pagination.page_size}
+//           onPageChange={handlePageChange}
+//           onRowsPerPageChange={handleRowsPerPageChange}
+//         />
+//       </Card>
+//     </Container>
+//   );
+// }
